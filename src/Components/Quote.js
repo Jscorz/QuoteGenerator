8bfd29b173161d566aed5a374f673180a9337765
Justin Scorzafava <jkscorzafava@gmail.com>
2022-03-28 13:50:42 -0600

    installed react icons and started basic styling of card element

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,12 +1,16 @@
 import React, { useState, useContext, useEffect } from 'react';
 import styled from 'styled-components';
-// import { useGlobalContext } from '../Context/context';
+import { BiRightArrowCircle } from 'react-icons/bi';
 
 const url = 'https://api.quotable.io/random';
 
 const Quote = () => {
   const [loading, setLoading] = useState(true);
   const [quote, setQuote] = useState([]);
+
+  const handleSubmit = () => {
+    fetchQuotes();
+  };
 
   const fetchQuotes = async () => {
     setLoading(true);
@@ -34,12 +38,21 @@ const Quote = () => {
   return (
     <Wrapper>
       <section>
-        <h1>Random Quote Generator</h1>
-        <h2>{quote.author}</h2>
-        <p>{quote.content}</p>
-        <div className='btn-container'>
-          <button type='button'>next quote</button>
-        </div>
+        <article className='card'>
+          <div className='card_container'>
+            <h1>Random Quote Generator</h1>
+            <h2>{quote.author}</h2>
+            <p>"{quote.content}"</p>
+            <div className='btn-container'>
+              <button type='button' onClick={handleSubmit}>
+                next quote{' '}
+                <span>
+                  <BiRightArrowCircle />
+                </span>
+              </button>
+            </div>
+          </div>
+        </article>
       </section>
     </Wrapper>
   );
@@ -49,12 +62,43 @@ export default Quote;
 
 const Wrapper = styled.section`
   section {
-    background-color: lightgrey;
+    background-color: #eeedde;
+
     min-width: 90%;
     min-height: 110vh;
 
     display: flex;
+    justify-content: center;
+    align-items: start;
+  }
+
+  .card {
+    width: 40%;
+    min-height: 60vh;
+    padding: 2rem;
+
+    display: flex;
+
+    justify-content: center;
+    align-items: space-between;
     flex-direction: column;
-    align-items: center;
+    border: 1px solid #203239;
+    &_container {
+      min-width: 100%;
+      border: 1px solid red;
+      padding: 2rem;
+    }
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.8rem;
+  }
+
+  p {
+    font-size: 1.8rem;
   }
 `;
