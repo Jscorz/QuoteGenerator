b0a931389dd400a584a58cb73d04091ca07a6297
Justin Scorzafava <jkscorzafava@gmail.com>
2022-03-28 12:59:06 -0600

    removed fetch from context and added it directly to quote file. displaying data

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import styled from 'styled-components';
+// import { useGlobalContext } from '../Context/context';
+
+const url = 'https://api.quotable.io/random';
 
 const Quote = () => {
+  const [loading, setLoading] = useState(true);
+  const [quote, setQuote] = useState([]);
+
+  const fetchQuotes = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${url}`);
+      const data = await response.json();
+      if (data) {
+        setQuote({ ...data });
+        setLoading(false);
+        return quote;
+      } else {
+        console.log('error');
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
   return (
     <Wrapper>
       <section>
-        <h1>Insert Quote Here</h1>
+        <h1>{quote.author}</h1>
+        <h1>{quote.content}</h1>
+        <h2>{quote.authorSlug}</h2>
       </section>
     </Wrapper>
   );
@@ -16,5 +47,7 @@ export default Quote;
 const Wrapper = styled.section`
   section {
     background-color: rgba(251, 86, 48, 0.8);
+    min-width: 90%;
+    min-height: 110vh;
   }
 `;
