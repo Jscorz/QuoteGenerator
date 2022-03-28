212eb6a7efb5213b2a011fb665b2e74088851645
Justin Scorzafava <jkscorzafava@gmail.com>
2022-03-28 15:25:50 -0600

    added basic styling for app

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -41,14 +41,11 @@ const Quote = () => {
         <article className='card'>
           <div className='card_container'>
             <h1>Random Quote Generator</h1>
-            <h2>{quote.author}</h2>
             <p>"{quote.content}"</p>
-            <div className='btn-container'>
-              <button type='button' onClick={handleSubmit}>
-                next quote{' '}
-                <span>
-                  <BiRightArrowCircle />
-                </span>
+            <h2>-{quote.author}</h2>
+            <div className='btn_container'>
+              <button type='button' onClick={handleSubmit} className='btn'>
+                <BiRightArrowCircle className='icon' />
               </button>
             </div>
           </div>
@@ -73,32 +70,59 @@ const Wrapper = styled.section`
   }
 
   .card {
-    width: 40%;
+    width: 50%;
     min-height: 60vh;
     padding: 2rem;
+    margin: 20vh 0 20vh 0;
 
     display: flex;
+    align-items: center;
+    text-align: center;
 
     justify-content: center;
     align-items: space-between;
     flex-direction: column;
     border: 1px solid #203239;
+    background-color: #203239;
+
     &_container {
       min-width: 100%;
-      border: 1px solid red;
+      border: 1px solid #e0ddaa;
       padding: 2rem;
     }
   }
 
   h1 {
     font-size: 2rem;
+    color #203239;
   }
 
   h2 {
     font-size: 1.8rem;
+    color #203239;
   }
 
   p {
     font-size: 1.8rem;
+    color #203239;
+  }
+
+  .btn {
+    border: none;
+    cursor: pointer;
+  }
+
+  .btn_container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border: none;
+  }
+
+  .icon {
+    font-size: 2rem;
+    color: #203239;
+    border: none;
   }
 `;
